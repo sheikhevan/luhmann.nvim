e35d26c155859c62b4c73eac1b821322f9cf1ba2
Evan Alvarez <evanfalvarez@tutamail.com>
2025-08-08 00:37:10 -0500

    bro can it just work

--- lua/zk-extras/init.lua
+++ lua/zk-extras/init.lua
@@ -1,5 +1,14 @@
 local M = {}
 
-function M.setup()
-    print("The code works!!!")
+function M.setup(opts)
+    local config = require("zk-extras.config")
+
+    local user_config = vim.tbl_deep_extend('force', config.default_config, opts or {})
+    M.config = user_config
+
+    if user_config.graph.enabled then
+        require('zk-extras.graph')
+    end
 end
+
+return M
